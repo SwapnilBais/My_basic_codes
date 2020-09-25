bca4b0468cac27a893850b3933f0a15ce94c62ed
Swapnil Bais <67828869+SwapnilBais@users.noreply.github.com>
2020-09-25 11:15:37 +0530

    Add files via upload

---
+++ Lesser_of_two_evens.py
@@ -0,0 +1,8 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 26 2018, 23:26:24) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> def animal_crackers(text):
+    wordlist=text.lower().split()
+    return wordlist[0][0]==wordlist[1][0]
+
+>>> 
